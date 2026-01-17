7f4ebbd765cea10f0d24b5be70d3908b5e615f4f
Gautam-Jha-13 <jgautam963@gmail.com>
2026-01-17 21:46:13 +0530

    E-Wallet Simulator

--- src/main/java/com/ewallet/wallet_service/exception/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.ewallet.wallet_service.exception;
-
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ewallet/wallet_service/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ewallet.wallet_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
